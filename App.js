a6c63d9ab6e3af840339ff6ffb3dc27e29c61084
Javier Ezpeleta <javierezpeleta@gmail.com>
2017-09-20 11:21:27 +0200

    tying out styled components!

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import styled from "styled-components/native"
+
+const CenterView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background: #333;
+`
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <CenterView>
+          <Text>TEST HERE!</Text>
+        </CenterView>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
