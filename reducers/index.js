bb04dbb0ae5e9bcd3e05aa5d769dad767720257a
Javier Ezpeleta <javierezpeleta@gmail.com>
2017-09-25 12:44:29 +0200

    properly adding each new question to the redux store

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,28 @@
 import { combineReducers } from "redux"
-import { SAVE_SCROLL_POSITION, SET_DECKS } from "./../actions"
+import { SAVE_SCROLL_POSITION, SET_DECKS, ADD_QUESTION } from "./../actions"
 
 function decks(state = {}, action) {
   switch (action.type) {
     case SET_DECKS:
       return action.decks
+    case ADD_QUESTION:
+      const newComment = {
+        question: action.question,
+        answer: action.answer,
+        deckId: action.deckId
+      }
+
+      console.log("in de moment")
+      console.log(state[action.deckId].questions)
+
+      return {
+        ...state,
+        [action.deckId]: {
+          ...state[action.deckId],
+          comments: state[action.deckId].questions.push(newComment)
+        }
+      }
+
     default:
       return state
   }
